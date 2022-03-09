fc22c6812ef38afdec7954add0fa5423ac9d5c17
Sikandar Ramzan <34787764+sikandar-ramzan@users.noreply.github.com>
2022-03-09 12:34:02 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Time-Stamper
+# API Project: Timestamp Microservice for FCC
+
+[Live on glitch](https://savory-desert-salesman.glitch.me/)
+
+The API endpoint is `GET [url]/api/timestamp/:date_string?`
+If the date string is **empty** the service uses the current timestamp.
+If the date string is **valid** the api returns a JSON e.g. `{"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}`.
+API accepts unix time.
+If the date string is **invalid** the api returns a JSON having the structure `{"error" : "Invalid Date" }`.
+
+#### Example usage:
+
+- https://savory-desert-salesman.glitch.me/api/timestamp/2015-12-25
+- https://savory-desert-salesman.glitch.me/api/timestamp/1451001600000
+
+#### Example output:
+
+- {"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+app.use(express.static("public"));
+
+// index
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// timestamp API
+app.get("/api/timestamp/:date?", (req, res) => {
+  const givenDate = req.params.date;
+  let date;
+
+  // check if no date provided
+  if (!givenDate) {
+    date = new Date();
+  } else {
+    // check if unix time:
+    //    number string multiplied by 1 gives this number, data string gives NaN
+    const checkUnix = givenDate * 1;
+    date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix);
+  }
+
+  //check if valid format
+  if (date == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+    res.json({ unix, utc });
+  }
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
